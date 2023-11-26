d8840ac0d11e462ad859f74c2c61849316e0bdde
Roboskeletron <wwwrt287@gmail.com>
2023-11-26 23:19:47 +0300

    load and bind configuration

---
+++ Shared/ITentikaTest.Common/Settings/Settings.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ITentikaTest.Common.Settings;
+
+public abstract class Settings
+{
+    public static T Load<T>(string key, IConfiguration? configuration = null)
+    {
+        var settings = (T)Activator.CreateInstance(typeof(T));
+        
+        ArgumentNullException.ThrowIfNull(settings);
+
+        SettingsFactory.Create(configuration).GetSection(key)
+            .Bind(settings, x => { x.BindNonPublicProperties = true; });
+
+        return settings;
+    }
+}
