c7a8c1973384d033fe2cd6cdb72687d2ef405164
Roboskeletron <wwwrt287@gmail.com>
2023-11-26 23:23:41 +0300

    bootstrap database

--- Data/ITentikaTest.Context/Bootstrapper.cs
+++ Data/ITentikaTest.Context/Bootstrapper.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ITentikaTest.Common.Settings;
+using ITentikaTest.Context;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,8 +9,18 @@ namespace Context;
 public static class Bootstrapper
 {
     public static IServiceCollection AddAppDbContext<TDbContext>(this IServiceCollection serviceCollection,
-        IConfiguration configuration = null) where TDbContext : DbContext
+        IConfiguration? configuration = null) where TDbContext : DbContext
     {
+        var settings = Settings.Load<DbSettings>($"Database{typeof(TDbContext)}", configuration);
+
+        serviceCollection.AddSingleton(settings);
+        
+        var dbInitOptionsDelegate = DbContextOptionsFactory.Configure(
+            settings.ConnectionString,
+            settings.Type);
+
+        serviceCollection.AddDbContextFactory<TDbContext>(dbInitOptionsDelegate);
+
         return serviceCollection;
     }
 }
