d9920601d7c181eac72d0f954a1d6beab2ba79fc
Roboskeletron <wwwrt287@gmail.com>
2023-11-27 17:31:15 +0300

    fluent validation added

---
+++ Shared/ITentikaTest.Common/Validators/IModelValidator.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation.Results;
+
+namespace ITentikaTest.Common.Validators;
+
+public interface IModelValidator<T> where T : class
+{
+    void Check(T model);
+    ValidationResult Validate(T model);
+}
