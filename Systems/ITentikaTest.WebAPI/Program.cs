bb9b2c77efe82507fe3487cc4392ef5fbc613cfc
Roboskeletron <wwwrt287@gmail.com>
2023-11-27 21:12:08 +0300

    controllers configuration

--- Systems/ITentikaTest.WebAPI/Program.cs
+++ Systems/ITentikaTest.WebAPI/Program.cs
@@ -5,9 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
 services.AddAppSwagger();
+services.AddAppControllers();
 
 var app = builder.Build();
 
 app.UseAppSwagger();
+app.UseAppControllers();
 
 app.Run();
