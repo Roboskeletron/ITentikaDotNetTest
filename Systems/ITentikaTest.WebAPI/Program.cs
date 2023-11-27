34687f3d4ffe5afa8bf8f7c45114062c839d970a
Roboskeletron <wwwrt287@gmail.com>
2023-11-27 18:52:49 +0300

    swagger configuration

--- Systems/ITentikaTest.WebAPI/Program.cs
+++ Systems/ITentikaTest.WebAPI/Program.cs
@@ -1,6 +1,13 @@
+using ITentikaTest.WebAPI.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddAppSwagger();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAppSwagger();
 
 app.Run();
