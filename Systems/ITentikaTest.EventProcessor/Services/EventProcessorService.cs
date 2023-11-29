723ec64b8e710a1b2f43956db0eec3a7ab5eb388
Roboskeletron <wwwrt287@gmail.com>
2023-11-29 19:00:48 +0300

    fix usings

--- Systems/ITentikaTest.EventProcessor/Services/EventProcessorService.cs
+++ Systems/ITentikaTest.EventProcessor/Services/EventProcessorService.cs
@@ -1,5 +1,4 @@
-﻿using Castle.Core.Internal;
-using Context.Entities.Event;
+﻿using Context.Entities.Event;
 using ITentikaTest.EventProcessor.Services.EventService;
 using ITentikaTest.EventProcessor.Services.Facories;
 
