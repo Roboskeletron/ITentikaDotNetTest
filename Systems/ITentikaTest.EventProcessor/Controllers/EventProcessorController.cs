0e31eda7a1f11c2372348e2227569f9ba8399690
Roboskeletron <wwwrt287@gmail.com>
2023-11-29 19:18:17 +0300

    docs added

--- Systems/ITentikaTest.EventProcessor/Controllers/EventProcessorController.cs
+++ Systems/ITentikaTest.EventProcessor/Controllers/EventProcessorController.cs
@@ -1,4 +1,6 @@
-﻿using Context.Entities.Event;
+﻿using System.Net;
+using Context.Entities.Event;
+using Context.Entities.Incident;
 using ITentikaTest.EventProcessor.Services.EventService;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,16 +17,29 @@ public class EventProcessorController : ControllerBase
         this.eventService = eventService;
     }
 
+    /// <summary>
+    /// Receive events endpoint
+    /// </summary>
+    /// <param name="generatedEvent">Received event</param>
+    /// <returns>Received event</returns>
     [Route("add")]
     [HttpPost]
+    [ProducesResponseType(typeof(Event), (int)HttpStatusCode.OK)]
     public async Task<IActionResult> AddEvent([FromBody] Event generatedEvent)
     {
         await eventService.PushEvent(generatedEvent);
         return Ok(generatedEvent);
     }
 
+    /// <summary>
+    /// Get created incidents list ordered by incident creation time
+    /// </summary>
+    /// <param name="offset">Offset from the first incident</param>
+    /// <param name="limit">Number of incidents to fetch</param>
+    /// <returns></returns>
     [Route("Incidents")]
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<Incident>), (int)HttpStatusCode.OK)]
     public async Task<IActionResult> GetIncidents([FromQuery] int offset = 0, [FromQuery] int limit = 10)
     {
         var incidents = await eventService.GetIncidents(offset, limit);
